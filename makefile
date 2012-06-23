68d8d0f7dedebc52474e481406ab98b199b56789
Adam Green <adamgrym@yahoo.com>
2012-06-23 01:48:26 -0700

    Adding CppUTest unit testing framework.
    Also created basic command line parsing module with unit tests.

---
+++ makefile
@@ -0,0 +1,28 @@
+# Copyright (C) 2012  Adam Green (https://github.com/adamgreen)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Directories to be built
+DIRS=CppUTest
+
+.PHONY: subdirs $(DIRS)
+
+subdirs:$(DIRS)
+
+clean: $(DIRS)
+
+# Recurse into each of the specified directories and perform a make
+$(DIRS):
+	$(MAKE) $(MAKECMDGOALS) -C $@
+
+# Run makefile which pulls mbed libraries down from mbed.org when necessary.
+install_mbed:
+	$(MAKE) -C external
---
+++ snap/tests/CommandLineTest.cpp
@@ -0,0 +1,81 @@
+/*  Copyright (C) 2012  Adam Green (https://github.com/adamgreen)
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+#include "string.h"
+
+// Include headers from C modules under test.
+extern "C"
+{
+#include "CommandLine.h"
+#include "util.h"
+}
+
+// Include C++ headers for test harness.
+#include "CppUTest/TestHarness.h"
+
+
+TEST_GROUP(CommandLine)
+{
+    const char*     m_argv[10];
+    CommandLine     m_CommandLine;
+    int             m_argc;
+    
+    void setup()
+    {
+        memset(m_argv, 0, sizeof(m_argv));
+        memset(&m_CommandLine, 0, sizeof(m_CommandLine));
+        m_argc = 0;
+    }
+
+    void teardown()
+    {
+    }
+
+    void addArg(const char* pArg)
+    {
+        CHECK(m_argc < (int)ARRAYSIZE(m_argv));
+        m_argv[m_argc++] = pArg;
+    }
+    
+    void validateSourceFiles(const char** pFirstSourceFileArg, int expectedSourceFileCount)
+    {
+        LONGS_EQUAL(expectedSourceFileCount, m_CommandLine.sourceFileCount);
+        CHECK(m_CommandLine.pSourceFiles != NULL);
+        for (int i = 0 ; i < expectedSourceFileCount ; i++)
+            POINTERS_EQUAL(pFirstSourceFileArg[i], m_CommandLine.pSourceFiles[i]);
+    }
+};
+
+
+TEST(CommandLine, NoParameters)
+{
+    LONGS_EQUAL(0, CommandLine_Init(&m_CommandLine, m_argc, m_argv));
+    LONGS_EQUAL(0, m_CommandLine.sourceFileCount);
+    CHECK(m_CommandLine.pSourceFiles == NULL);
+}
+
+TEST(CommandLine, OneSourceFilename)
+{
+    addArg("SOURCE1.S");
+    
+    LONGS_EQUAL(0, CommandLine_Init(&m_CommandLine, m_argc, m_argv));
+    validateSourceFiles(m_argv, 1);
+}
+
+TEST(CommandLine, TwoSourceFilenames)
+{
+    addArg("SOURCE1.S");
+    addArg("SOURCE2.S");
+    
+    LONGS_EQUAL(0, CommandLine_Init(&m_CommandLine, m_argc, m_argv));
+    validateSourceFiles(m_argv, 2);
+}
