9bf2cbba2510156f21418ce9e2ca7603e29e2635
Adam Green <adamgrym@yahoo.com>
2012-07-18 00:06:30 -0700

    Refactored AddressingMode module.
    Pulled a lot of code out of main AddressingMode_Eval() function and placed in
    smaller functions.  I think this makes the intent of the main routine easier to
    understand and maintain.

--- libsnap/src/AddressingMode.c
+++ libsnap/src/AddressingMode.c
@@ -17,139 +17,82 @@
 #include "AssemblerPriv.h"
 
 
+typedef struct CharLocations
+{
+    const char* pStart;
+    const char* pEnd;
+    const char* pComma;
+    const char* pOpenParen;
+    const char* pCloseParen;
+}
+CharLocations;
+
+
+static CharLocations initCharLocations(SizedString* pOperandsString);
+static AddressingMode initializedAddressingModeStruct(AddressingModes mode);
 static int usesImpliedAddressing(const char* pOperands);
-static int usesImmediateAddressing(const char* pOperands);
+static AddressingMode impliedAddressing(void);
+static int usesIndexedIndirectAddressing(CharLocations* pLocations);
+static int hasParensAndComma(CharLocations* pLocations);
+static int hasOpeningParenAtBeginning(CharLocations* pLocations);
+static int hasClosingParenAtEnding(CharLocations* pLocations);
+static int isCommaAfterOpeningParen(CharLocations* pLocations);
+static int isClosingParenAfterComma(CharLocations* pLocations);
+static AddressingMode indexedIndirectAddressing(Assembler* pAssembler, SizedString* pOperandsString);
+static int usesIndirectIndexedAddressing(CharLocations* pLocations);
+static int isCommaAfterClosingParen(CharLocations* pLocations);
+static AddressingMode indirectIndexedAddressing(Assembler* pAssembler, SizedString* pOperandsString);
+static int usesIndirectAddressing(CharLocations* pLocations);
+static int hasParensAndNoComma(CharLocations* pLocations);
+static AddressingMode indirectAddressing(Assembler* pAssembler, SizedString* pOperandsString);
+static int usesIndexedAddressing(CharLocations* pLocations);
+static int hasCommaAndNoParens(CharLocations* pLocations);
+static AddressingMode indexedAddressing(Assembler* pAssembler, SizedString* pOperandsString);
+static int hasNoCommaOrParens(CharLocations* pLocations);
+static AddressingMode immediateOrAbsoluteAddressing(Assembler* pAssembler, SizedString* pOperandsString);
+static int usesImmediateAddressing(SizedString* pOperandsString);
 __throws AddressingMode AddressingMode_Eval(Assembler* pAssembler, const char* pOperands)
 {
-    AddressingMode addressingMode;
     SizedString    operandsString = SizedString_InitFromString(pOperands);
-    const char*    pComma = SizedString_strchr(&operandsString, ',');
-    const char*    pOpenParen = SizedString_strchr(&operandsString, '(');
-    const char*    pCloseParen = SizedString_strchr(&operandsString, ')');
+    CharLocations  locations = initCharLocations(&operandsString);
+    AddressingMode addressingMode = initializedAddressingModeStruct(ADDRESSING_MODE_INVALID);
 
-    memset(&addressingMode, 0, sizeof(addressingMode));
-    addressingMode.mode = ADDRESSING_MODE_INVALID;
     if (usesImpliedAddressing(pOperands))
-    {
-        addressingMode.mode = ADDRESSING_MODE_IMPLIED;
-        return addressingMode;
-    }
-    else if (pOpenParen && pCloseParen && pComma && 
-             pOpenParen == operandsString.pString && pComma > pOpenParen && pCloseParen > pComma)
-    {
-        SizedString beforeOpenParen;
-        SizedString afterOpenParen;
-        SizedString beforeComma;
-        SizedString afterComma;
-
-        SizedString_SplitString(&operandsString, '(', &beforeOpenParen, &afterOpenParen);
-        SizedString_SplitString(&afterOpenParen, ',', &beforeComma, &afterComma);
-        if (0 == SizedString_strcasecmp(&afterComma, "X)"))
-        {
-            __try
-                addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeComma);
-            __catch
-                __rethrow_and_return(addressingMode);
-
-            addressingMode.mode = ADDRESSING_MODE_INDEXED_INDIRECT;
-            return addressingMode;
-        }
-    }
-    else if (pOpenParen && pCloseParen && pComma && 
-             pOpenParen == operandsString.pString && pComma > pOpenParen && pComma > pCloseParen)
-    {
-        SizedString beforeOpenParen;
-        SizedString afterOpenParen;
-        SizedString beforeCloseParen;
-        SizedString afterCloseParen;
-
-        SizedString_SplitString(&operandsString, '(', &beforeOpenParen, &afterOpenParen);
-        SizedString_SplitString(&afterOpenParen, ')', &beforeCloseParen, &afterCloseParen);
-        if (0 == SizedString_strcasecmp(&afterCloseParen, ",Y"))
-        {
-            __try
-                addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeCloseParen);
-            __catch
-                __rethrow_and_return(addressingMode);
-                
-            if (addressingMode.expression.type != TYPE_ZEROPAGE)
-            {
-                LOG_ERROR(pAssembler, "'%.*s' isn't in page zero as required for indirect indexed addressing.", 
-                          beforeCloseParen.stringLength, beforeCloseParen.pString);
-                __throw_and_return(invalidArgumentException, addressingMode);
-            }
-
-            addressingMode.mode = ADDRESSING_MODE_INDIRECT_INDEXED;
-            return addressingMode;
-        }
-    }
-    else if (pOpenParen && pCloseParen && !pComma && pOpenParen < pCloseParen)
-    {
-        SizedString beforeOpenParen;
-        SizedString afterOpenParen;
-        SizedString beforeCloseParen;
-        SizedString afterCloseParen;
-
-        SizedString_SplitString(&operandsString, '(', &beforeOpenParen, &afterOpenParen);
-        SizedString_SplitString(&afterOpenParen, ')', &beforeCloseParen, &afterCloseParen);
-        __try
-            addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeCloseParen);
-        __catch
-            __rethrow_and_return(addressingMode);
-            
-        addressingMode.mode = ADDRESSING_MODE_INDIRECT;
-        return addressingMode;
-    }
-    else if (pComma && !pOpenParen && !pCloseParen)
-    {
-        SizedString beforeComma;
-        SizedString afterComma;
-        
-        SizedString_SplitString(&operandsString, ',', &beforeComma, &afterComma);
-        if (0 == SizedString_strcasecmp(&afterComma, "X"))
-        {
-            __try
-                addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeComma);
-            __catch
-                __rethrow_and_return(addressingMode);
-                
-            addressingMode.mode = ADDRESSING_MODE_ABSOLUTE_INDEXED_X;
-            return addressingMode;
-        }
-        else if (0 == SizedString_strcasecmp(&afterComma, "Y"))
-        {
-            __try
-                addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeComma);
-            __catch
-                __rethrow_and_return(addressingMode);
-
-            addressingMode.mode = ADDRESSING_MODE_ABSOLUTE_INDEXED_Y;
-            return addressingMode;
-        }
-    }
-    else if (usesImmediateAddressing(pOperands))
-    {
-        __try
-            addressingMode.expression = ExpressionEval(pAssembler, pOperands);
-        __catch
-            __rethrow_and_return(addressingMode);
-            
-        addressingMode.mode = ADDRESSING_MODE_IMMEDIATE;
-        return addressingMode;
-    }
+        return impliedAddressing();
+    else if (usesIndexedIndirectAddressing(&locations))
+        return indexedIndirectAddressing(pAssembler, &operandsString);
+    else if (usesIndirectIndexedAddressing(&locations))
+        return indirectIndexedAddressing(pAssembler, &operandsString);
+    else if (usesIndirectAddressing(&locations))
+        return indirectAddressing(pAssembler, &operandsString);
+    else if (usesIndexedAddressing(&locations))
+        return indexedAddressing(pAssembler, &operandsString);
+    else if (hasNoCommaOrParens(&locations))
+        return immediateOrAbsoluteAddressing(pAssembler, &operandsString);
     else
-    {
-        __try
-            addressingMode.expression = ExpressionEval(pAssembler, pOperands);
-        __catch
-            __rethrow_and_return(addressingMode);
-            
-        addressingMode.mode = ADDRESSING_MODE_ABSOLUTE;
-        return addressingMode;
-    }
+        __throw_and_return(invalidArgumentException, addressingMode);
+}
+
+static CharLocations initCharLocations(SizedString* pOperandsString)
+{
+    CharLocations locations;
+
+    locations.pStart = pOperandsString->pString;
+    locations.pEnd = pOperandsString->pString + pOperandsString->stringLength - 1;
+    locations.pComma = SizedString_strchr(pOperandsString, ',');
+    locations.pOpenParen = SizedString_strchr(pOperandsString, '(');
+    locations.pCloseParen = SizedString_strchr(pOperandsString, ')');
     
-    addressingMode.mode = ADDRESSING_MODE_INVALID;
-    __throw_and_return(invalidArgumentException, addressingMode);
+    return locations;
+}
+
+static AddressingMode initializedAddressingModeStruct(AddressingModes mode)
+{
+    AddressingMode addressingMode;
+    
+    memset(&addressingMode, 0, sizeof(addressingMode));
+    addressingMode.mode = mode;
+    return addressingMode;
 }
 
 static int usesImpliedAddressing(const char* pOperands)
@@ -157,7 +100,200 @@ static int usesImpliedAddressing(const char* pOperands)
     return pOperands == NULL;
 }
 
-static int usesImmediateAddressing(const char* pOperands)
+static AddressingMode impliedAddressing(void)
 {
-    return pOperands[0] == '#';
+    return initializedAddressingModeStruct(ADDRESSING_MODE_IMPLIED);
+}
+
+static int usesIndexedIndirectAddressing(CharLocations* pLocations)
+{
+    return hasParensAndComma(pLocations) && 
+           hasOpeningParenAtBeginning(pLocations) && hasClosingParenAtEnding(pLocations) &&
+           isCommaAfterOpeningParen(pLocations) && isClosingParenAfterComma(pLocations);
+}
+
+static int hasParensAndComma(CharLocations* pLocations)
+{
+    return pLocations->pOpenParen && pLocations->pCloseParen && pLocations->pComma;
+}
+
+static int hasOpeningParenAtBeginning(CharLocations* pLocations)
+{
+    return pLocations->pOpenParen == pLocations->pStart;
+}
+
+static int hasClosingParenAtEnding(CharLocations* pLocations)
+{
+    return pLocations->pCloseParen == pLocations->pEnd;
+}
+
+static int isCommaAfterOpeningParen(CharLocations* pLocations)
+{
+    return pLocations->pComma > pLocations->pOpenParen;
+}
+
+static int isClosingParenAfterComma(CharLocations* pLocations)
+{
+    return pLocations->pCloseParen > pLocations->pComma;
+}
+
+static AddressingMode indexedIndirectAddressing(Assembler* pAssembler, SizedString* pOperandsString)
+{
+    AddressingMode addressingMode = initializedAddressingModeStruct(ADDRESSING_MODE_INVALID);
+    SizedString    beforeOpenParen;
+    SizedString    afterOpenParen;
+    SizedString    beforeComma;
+    SizedString    afterComma;
+
+    SizedString_SplitString(pOperandsString, '(', &beforeOpenParen, &afterOpenParen);
+    SizedString_SplitString(&afterOpenParen, ',', &beforeComma, &afterComma);
+    if (0 != SizedString_strcasecmp(&afterComma, "X)"))
+        __throw_and_return(invalidArgumentException, addressingMode);
+
+    __try
+        addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeComma);
+    __catch
+        __rethrow_and_return(addressingMode);
+
+    addressingMode.mode = ADDRESSING_MODE_INDEXED_INDIRECT;
+    return addressingMode;
+}
+
+static int usesIndirectIndexedAddressing(CharLocations* pLocations)
+{
+    return hasParensAndComma(pLocations) && hasOpeningParenAtBeginning(pLocations) && 
+           isCommaAfterOpeningParen(pLocations) && isCommaAfterClosingParen(pLocations);
+}
+
+static int isCommaAfterClosingParen(CharLocations* pLocations)
+{
+    return !isClosingParenAfterComma(pLocations);
+}
+
+static AddressingMode indirectIndexedAddressing(Assembler* pAssembler, SizedString* pOperandsString)
+{
+    AddressingMode addressingMode = initializedAddressingModeStruct(ADDRESSING_MODE_INVALID);
+    SizedString    beforeOpenParen;
+    SizedString    afterOpenParen;
+    SizedString    beforeCloseParen;
+    SizedString    afterCloseParen;
+
+    SizedString_SplitString(pOperandsString, '(', &beforeOpenParen, &afterOpenParen);
+    SizedString_SplitString(&afterOpenParen, ')', &beforeCloseParen, &afterCloseParen);
+    if (0 != SizedString_strcasecmp(&afterCloseParen, ",Y"))
+        __throw_and_return(invalidArgumentException, addressingMode);
+        
+    __try
+        addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeCloseParen);
+    __catch
+        __rethrow_and_return(addressingMode);
+        
+    if (addressingMode.expression.type != TYPE_ZEROPAGE)
+    {
+        LOG_ERROR(pAssembler, "'%.*s' isn't in page zero as required for indirect indexed addressing.", 
+                  beforeCloseParen.stringLength, beforeCloseParen.pString);
+        __throw_and_return(invalidArgumentException, addressingMode);
+    }
+
+    addressingMode.mode = ADDRESSING_MODE_INDIRECT_INDEXED;
+    return addressingMode;
+}
+
+static int usesIndirectAddressing(CharLocations* pLocations)
+{
+    return hasParensAndNoComma(pLocations) && 
+           hasOpeningParenAtBeginning(pLocations) && hasClosingParenAtEnding(pLocations);
+}
+
+static int hasParensAndNoComma(CharLocations* pLocations)
+{
+    return pLocations->pOpenParen && pLocations->pCloseParen && !pLocations->pComma;
+}
+
+static AddressingMode indirectAddressing(Assembler* pAssembler, SizedString* pOperandsString)
+{
+    AddressingMode addressingMode = initializedAddressingModeStruct(ADDRESSING_MODE_INVALID);
+    SizedString beforeOpenParen;
+    SizedString afterOpenParen;
+    SizedString beforeCloseParen;
+    SizedString afterCloseParen;
+
+    SizedString_SplitString(pOperandsString, '(', &beforeOpenParen, &afterOpenParen);
+    SizedString_SplitString(&afterOpenParen, ')', &beforeCloseParen, &afterCloseParen);
+    __try
+        addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeCloseParen);
+    __catch
+        __rethrow_and_return(addressingMode);
+        
+    addressingMode.mode = ADDRESSING_MODE_INDIRECT;
+    return addressingMode;
+}
+
+static int usesIndexedAddressing(CharLocations* pLocations)
+{
+    return hasCommaAndNoParens(pLocations);
+}
+
+static int hasCommaAndNoParens(CharLocations* pLocations)
+{
+    return pLocations->pComma && !pLocations->pOpenParen && !pLocations->pCloseParen;
+}
+
+static AddressingMode indexedAddressing(Assembler* pAssembler, SizedString* pOperandsString)
+{
+    AddressingMode addressingMode = initializedAddressingModeStruct(ADDRESSING_MODE_INVALID);
+    SizedString    beforeComma;
+    SizedString    afterComma;
+    
+    SizedString_SplitString(pOperandsString, ',', &beforeComma, &afterComma);
+    if (0 == SizedString_strcasecmp(&afterComma, "X"))
+        addressingMode.mode = ADDRESSING_MODE_ABSOLUTE_INDEXED_X;
+    else if (0 == SizedString_strcasecmp(&afterComma, "Y"))
+        addressingMode.mode = ADDRESSING_MODE_ABSOLUTE_INDEXED_Y;
+    else
+        __throw_and_return(invalidArgumentException, addressingMode);
+    
+    __try
+    {
+        addressingMode.expression = ExpressionEvalSizedString(pAssembler, &beforeComma);
+    }
+    __catch
+    {
+        addressingMode.mode = ADDRESSING_MODE_INVALID;
+        __rethrow_and_return(addressingMode);
+    }
+            
+    return addressingMode;
+}
+
+static int hasNoCommaOrParens(CharLocations* pLocations)
+{
+    return !pLocations->pComma && !pLocations->pOpenParen && !pLocations->pCloseParen;
+}
+
+static AddressingMode immediateOrAbsoluteAddressing(Assembler* pAssembler, SizedString* pOperandsString)
+{
+    AddressingMode addressingMode = initializedAddressingModeStruct(ADDRESSING_MODE_INVALID);
+    
+    if (usesImmediateAddressing(pOperandsString))
+        addressingMode.mode = ADDRESSING_MODE_IMMEDIATE;
+    else
+        addressingMode.mode = ADDRESSING_MODE_ABSOLUTE;
+
+    __try
+    {
+        addressingMode.expression = ExpressionEvalSizedString(pAssembler, pOperandsString);
+    }
+    __catch
+    {
+        addressingMode.mode = ADDRESSING_MODE_INVALID;
+        __rethrow_and_return(addressingMode);
+    }
+
+    return addressingMode;
+}
+
+static int usesImmediateAddressing(SizedString* pOperandsString)
+{
+    return pOperandsString->pString[0] == '#';
 }
