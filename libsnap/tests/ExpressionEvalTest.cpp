ca9b3744d7bf2d9afd2f2750cea5e023cf2dbef6
Adam Green <adamgrym@yahoo.com>
2012-09-06 13:11:21 -0700

    Refactor ExpressionEvalTest unit tests.
    Attempted to make code a bit shorter and make the intent of some of the tests
    more clear.

--- libsnap/tests/ExpressionEvalTest.cpp
+++ libsnap/tests/ExpressionEvalTest.cpp
@@ -58,7 +58,7 @@ TEST_GROUP(ExpressionEval)
         return &m_string;
     }
     
-    void validateFailureMessage(const char* pExpectedErrorMessage, int expectedExceptionCode)
+    void validateFailureMessageAndThrownException(const char* pExpectedErrorMessage, int expectedExceptionCode)
     {
         STRCMP_EQUAL(pExpectedErrorMessage, printfSpy_GetLastOutput());
         POINTERS_EQUAL(stderr, printfSpy_GetLastFile());
@@ -74,252 +74,231 @@ TEST_GROUP(ExpressionEval)
         CHECK(m_pAssembler != NULL);
         Assembler_Run(m_pAssembler);
     }
+    
+    void validateExpression(ExpressionType expectedType, unsigned short expectedValue)
+    {
+        LONGS_EQUAL(expectedType, m_expression.type);
+        LONGS_EQUAL(expectedValue, m_expression.value);
+    }
 };
 
 
 TEST(ExpressionEval, EvaluateHexLiteral)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("$AF09"));
-    LONGS_EQUAL(0xaf09, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0xaf09);
 }
 
 TEST(ExpressionEval, EvaluateInvalidHexDigit)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("$AG")) );
-    validateFailureMessage("filename:0: error: 'G' is unexpected operator.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: 'G' is unexpected operator.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateHexValueTooLong)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("$12345")) );
-    validateFailureMessage("filename:0: error: Hexadecimal number '$12345' doesn't fit in 16-bits.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: Hexadecimal number '$12345' doesn't fit in 16-bits.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateHexImmediate)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#$60"));
-    LONGS_EQUAL(0x60, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0x60);
 }
 
 TEST(ExpressionEval, EvaluateBinaryLiteral)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("%1010111100001001"));
-    LONGS_EQUAL(0xaf09, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0xaf09);
 }
 
 TEST(ExpressionEval, EvaluateInvalidBinaryDigit)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("%12")) );
-    validateFailureMessage("filename:0: error: '2' is unexpected operator.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: '2' is unexpected operator.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateBinaryValueTooLong)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("%11110000111100001")) );
-    validateFailureMessage("filename:0: error: Binary number '%11110000111100001' doesn't fit in 16-bits.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: Binary number '%11110000111100001' doesn't fit in 16-bits.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateBinaryImmediate)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#%11110000"));
-    LONGS_EQUAL(0xF0, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0xF0);
 }
 
 TEST(ExpressionEval, EvaluateDecimalLiteral)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("65535"));
-    LONGS_EQUAL(0xffff, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0xffff);
 }
 
 TEST(ExpressionEval, EvaluateInvalidDecimalDigit)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("1f")) );
-    validateFailureMessage("filename:0: error: 'f' is unexpected operator.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: 'f' is unexpected operator.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateDecimalValueTooLong)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("65536")) );
-    validateFailureMessage("filename:0: error: Decimal number '65536' doesn't fit in 16-bits.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: Decimal number '65536' doesn't fit in 16-bits.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateDecimalImmediate)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#255"));
-    LONGS_EQUAL(0xFF, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0xFF);
 }
 
 TEST(ExpressionEval, ImmediateValueLargerThan8Bits)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#$100"));
-    LONGS_EQUAL(0x100, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0x100);
 }
 
 TEST(ExpressionEval, ImmediateValueLargerThan8BitsWithLessThanPrefix)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#<$100"));
-    LONGS_EQUAL(0x00, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0x100 & 0xff);
 }
 
 TEST(ExpressionEval, ImmediateValueLargerThan8BitsWithGreaterThanPrefix)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#>$100"));
-    LONGS_EQUAL(0x01, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0x100 >> 8);
 }
 
 TEST(ExpressionEval, ImmediateExpressionWithHighByteExtractionOnLongerExpression)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#>$D600-$400"));
-    LONGS_EQUAL(0xD2, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, (0xD600-0x400) >> 8);
 }
 
 TEST(ExpressionEval, ImmediateValueLargerThan8BitsWithCaretPrefix)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#^$100"));
-    LONGS_EQUAL(0x01, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 0x0100 >> 8);
 }
 
 TEST(ExpressionEval, InvalidUseOfLessThanPrefixOnNonImmediateValue)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("<$100")) );
-    validateFailureMessage("filename:0: error: '$' is unexpected operator.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: '$' is unexpected operator.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, InvalidUseOfGreaterThanPrefixOnNonImmediateValue)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString(">$100")) );
-    validateFailureMessage("filename:0: error: '$' is unexpected operator.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: '$' is unexpected operator.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, InvalidUseOfCaretPrefixOnNonImmediateValue)
 {
     __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("^$100")) );
-    validateFailureMessage("filename:0: error: '$' is unexpected operator.\n", invalidArgumentException);
+    validateFailureMessageAndThrownException("filename:0: error: '$' is unexpected operator.\n", invalidArgumentException);
 }
 
 TEST(ExpressionEval, EvaluateSingleQuotedASCII)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#'a'"));
-    LONGS_EQUAL('a', m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 'a');
 }
 
 TEST(ExpressionEval, EvaluateSingleQuotedPrefixOnlyASCII)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#'a+1"));
-    LONGS_EQUAL('a'+1, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 'a'+1);
 }
 
 TEST(ExpressionEval, EvaluateDoubleQuotedASCII)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#\"a\""));
-    LONGS_EQUAL('a' | 0x80, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, 'a' | 0x80);
 }
 
 TEST(ExpressionEval, EvaluateDoubleQuotedPrefixOnlyASCII)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("#\"a+1"));
-    LONGS_EQUAL(('a' | 0x80)+1, m_expression.value);
-    LONGS_EQUAL(TYPE_IMMEDIATE, m_expression.type);
+    validateExpression(TYPE_IMMEDIATE, ('a' | 0x80)+1);
 }
 
 TEST(ExpressionEval, AsteriskInExpression)
 {
     setupAssemblerModule(" org $800\n");
     m_expression = ExpressionEval(m_pAssembler, toSizedString("*"));
-    LONGS_EQUAL(0x800, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0x800);
 }
 
 TEST(ExpressionEval, EvaluateAddition)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("2+3"));
-    LONGS_EQUAL(5, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 2+3);
 }
 
 TEST(ExpressionEval, EvaluateSubtraction)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("3-2"));
-    LONGS_EQUAL(1, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 3-2);
 }
 
 TEST(ExpressionEval, EvaluateMultiplication)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("3*2"));
-    LONGS_EQUAL(6, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 3*2);
 }
 
 TEST(ExpressionEval, EvaluateDivision)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("3/2"));
-    LONGS_EQUAL(1, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 3/2);
 }
 
 TEST(ExpressionEval, EvaluateUnarySubtraction)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("-2"));
-    LONGS_EQUAL(0xFFFE, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0xFFFE);
 }
 
 TEST(ExpressionEval, EvaluateUnarySubtractionInMiddleOfExpression)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("2--2"));
-    LONGS_EQUAL(2+2, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 2- -2);
 }
 
 TEST(ExpressionEval, EvaluateNoOperatorPrecedenceExample)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("2+3*5"));
-    LONGS_EQUAL(25, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, (2+3)*5);
 }
 
 TEST(ExpressionEval, EvaluateXOR)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("$ff!$80"));
-    LONGS_EQUAL(0x7f, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 0x7f);
 }
 
 TEST(ExpressionEval, EvaluateOR)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("$00.$80"));
-    LONGS_EQUAL(0x80, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 0x80);
 }
 
 TEST(ExpressionEval, EvaluateAND)
 {
     m_expression = ExpressionEval(m_pAssembler, toSizedString("$af&$f0"));
-    LONGS_EQUAL(0xa0, m_expression.value);
-    LONGS_EQUAL(TYPE_ZEROPAGE, m_expression.type);
+    validateExpression(TYPE_ZEROPAGE, 0xaf&0xf0);
 }
 
 TEST(ExpressionEval, ForwardLabelReference)
 {
     static const char labelName[] = "fwd_label";
     m_expression = ExpressionEval(m_pAssembler, toSizedString(labelName));
-    LONGS_EQUAL(0x0, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0x0);
     CHECK_TRUE(m_expression.flags & EXPRESSION_FLAG_FORWARD_REFERENCE);
     
     Symbol* pSymbol = Assembler_FindLabel(m_pAssembler, toSizedString(labelName));
@@ -333,8 +312,7 @@ TEST(ExpressionEval, ForwardLabelReferenceAsFirstTerm)
 {
     static const char labelName[] = "fwd_label+1";
     m_expression = ExpressionEval(m_pAssembler, toSizedString(labelName));
-    LONGS_EQUAL(0x1, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0x1);
     CHECK_TRUE(m_expression.flags & EXPRESSION_FLAG_FORWARD_REFERENCE);
 }
 
@@ -342,8 +320,7 @@ TEST(ExpressionEval, ForwardLabelReferenceAsSecondTerm)
 {
     static const char labelName[] = "1+fwd_label";
     m_expression = ExpressionEval(m_pAssembler, toSizedString(labelName));
-    LONGS_EQUAL(0x1, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0x1);
     CHECK_TRUE(m_expression.flags & EXPRESSION_FLAG_FORWARD_REFERENCE);
 }
 
@@ -352,8 +329,7 @@ TEST(ExpressionEval, BackwardLabelReference)
     static const char labelName[] = "backLabel";
     setupAssemblerModule("backLabel equ $a55a\n");
     m_expression = ExpressionEval(m_pAssembler, toSizedString(labelName));
-    LONGS_EQUAL(0xa55a, m_expression.value);
-    LONGS_EQUAL(TYPE_ABSOLUTE, m_expression.type);
+    validateExpression(TYPE_ABSOLUTE, 0xa55a);
     CHECK_FALSE(m_expression.flags & EXPRESSION_FLAG_FORWARD_REFERENCE);
 
     Symbol* pSymbol = Assembler_FindLabel(m_pAssembler, toSizedString(labelName));
@@ -366,7 +342,7 @@ TEST(ExpressionEval, BackwardLabelReference)
 TEST(ExpressionEval, FailAllocationOnForwardLabelReference)
 {
     MallocFailureInject_FailAllocation(1);
-    __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("fwd_label")) );
+        __try_and_catch( m_expression = ExpressionEval(m_pAssembler, toSizedString("fwd_label")) );
     MallocFailureInject_Restore();
 
     LONGS_EQUAL(outOfMemoryException, getExceptionCode());
