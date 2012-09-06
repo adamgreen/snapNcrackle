3ee1168e09b35db64b070574c0f7b0be445ab35c
Adam Green <adamgrym@yahoo.com>
2012-09-05 23:31:19 -0700

    Added ListFile module unit test.
    After commenting out the Assembler module unit tests, I noticed that the code
    coverage for the ListFile module fell below 100%.  I needed to add a test which
    generated more than 3 bytes per line of assembly language code.

--- libsnap/tests/ListFileTest.cpp
+++ libsnap/tests/ListFileTest.cpp
@@ -133,3 +133,22 @@ TEST(ListFile, OutputLineWithAddressAndThreeMachineCodeBytes)
     POINTERS_EQUAL(stdout, printfSpy_GetLastFile());
     STRCMP_EQUAL("0803: AD C0 08     5  LDA $C008\n", printfSpy_GetLastOutput());
 }
+
+TEST(ListFile, OutputLineWithAddressAndFourMachineCodeBytes)
+{
+    m_pListFile = ListFile_Create(stdout);
+
+    m_lineInfo.pLineText = " DS 4";
+    m_lineInfo.lineNumber = 1;
+    m_lineInfo.address = 0x0800;
+    m_lineInfo.machineCodeSize = 4;
+    m_lineInfo.pMachineCode[0] = 0x00;
+    m_lineInfo.pMachineCode[1] = 0x00;
+    m_lineInfo.pMachineCode[2] = 0x00;
+    m_lineInfo.pMachineCode[3] = 0x00;
+    ListFile_OutputLine(m_pListFile, &m_lineInfo);
+
+    POINTERS_EQUAL(stdout, printfSpy_GetLastFile());
+    STRCMP_EQUAL("0800: 00 00 00     1  DS 4\n", printfSpy_GetPreviousOutput());
+    STRCMP_EQUAL("0803: 00      \n", printfSpy_GetLastOutput());
+}
