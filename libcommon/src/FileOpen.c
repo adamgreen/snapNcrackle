079ef9424e007aaf67c521edfe9dfd19a255decc
Adam Green <adamgr@foo.bar>
2013-03-25 23:26:55 -0700

    Remove dynamic memory allocations from FileOpen().

--- libcommon/src/FileOpen.c
+++ libcommon/src/FileOpen.c
@@ -14,13 +14,14 @@
 #include "FileOpen.h"
 #include "FileOpenTest.h"
 #include "string.h"
+#include "util.h"
 
 typedef struct
 {
-    char*  pDirectoryName;
-    char*  pFilename;
-    char*  pActualFilename;
     size_t filenameLength;
+    char directoryName[PATH_LENGTH];
+    char filename[PATH_LENGTH];
+    char actualFilename[PATH_LENGTH];
 } FilenameParts;
 
 typedef struct
@@ -41,41 +42,38 @@ typedef struct
 static FilenameParts initFilenameParts(const char* pFilename);
 static FilenameInfo determineFilenameInfo(const char* pFilename);
 static FilenamePointers findLastSlashAndEndOfFilename(const char* pFilename);
-static void* throwingMalloc(size_t size);
-static void freeFilenameParts(FilenameParts* pThis);
+static void throwIfLengthTooLong(int length, int limit);
 static const char* findFilenameCaseInsensitive(FilenameParts* pThis);
 __throws FILE* FileOpen(const char* pFilename, const char* pMode)
 {
-    FilenameParts filenameParts = initFilenameParts(pFilename);
-    FILE* pReturn = fopen(findFilenameCaseInsensitive(&filenameParts), pMode);
-    freeFilenameParts(&filenameParts);
-
-    return pReturn;
+    FilenameParts filenameParts;
+    __try
+    {
+        filenameParts = initFilenameParts(pFilename);
+    }
+    __catch
+    {
+        clearExceptionCode();
+        return NULL;
+    }
+    return fopen(findFilenameCaseInsensitive(&filenameParts), pMode);
 }
 
 static FilenameParts initFilenameParts(const char* pFilename)
 {
-    FilenameParts    filenameParts = { NULL, NULL, NULL, 0 };
+    FilenameParts    filenameParts = { 0, "", "", "" };
     
-    __try
-    {
-        FilenameInfo filenameInfo = determineFilenameInfo(pFilename);
-        
-        filenameParts.pDirectoryName = throwingMalloc(filenameInfo.directoryNameLength + 1);
-        filenameParts.pFilename = throwingMalloc(filenameInfo.filenameLength + 1);
-        filenameParts.pActualFilename = throwingMalloc(filenameInfo.filenameLength + 1);
+    FilenameInfo filenameInfo = determineFilenameInfo(pFilename);
     
-        memcpy(filenameParts.pDirectoryName, filenameInfo.pDirectoryName, filenameInfo.directoryNameLength);
-        filenameParts.pDirectoryName[filenameInfo.directoryNameLength] = '\0';
-        memcpy(filenameParts.pFilename, filenameInfo.pFilename, filenameInfo.filenameLength + 1);
-        filenameParts.filenameLength = filenameInfo.filenameLength;
-        memcpy(filenameParts.pActualFilename, filenameInfo.pFilename, filenameInfo.filenameLength + 1);
-    }
-    __catch
-    {
-        freeFilenameParts(&filenameParts);
-        __rethrow;
-    }
+    throwIfLengthTooLong(filenameInfo.filenameLength, sizeof(filenameParts.filename));
+    throwIfLengthTooLong(filenameInfo.directoryNameLength, sizeof(filenameParts.directoryName));
+    
+    memcpy(filenameParts.directoryName, filenameInfo.pDirectoryName, filenameInfo.directoryNameLength);
+    filenameParts.directoryName[filenameInfo.directoryNameLength] = '\0';
+    memcpy(filenameParts.filename, filenameInfo.pFilename, filenameInfo.filenameLength + 1);
+    filenameParts.filenameLength = filenameInfo.filenameLength;
+    memcpy(filenameParts.actualFilename, filenameInfo.pFilename, filenameInfo.filenameLength + 1);
+
     return filenameParts;
 }
 
@@ -117,12 +115,10 @@ static FilenamePointers findLastSlashAndEndOfFilename(const char* pFilename)
     return filenamePointers;
 }
 
-static void* throwingMalloc(size_t size)
+static void throwIfLengthTooLong(int length, int limit)
 {
-    void* pReturn = malloc(size);
-    if (!pReturn)
+    if (length >= limit)
         __throw(outOfMemoryException);
-    return pReturn;
 }
 
 static const char* findFilenameCaseInsensitive(FilenameParts* pThis)
@@ -130,26 +126,19 @@ static const char* findFilenameCaseInsensitive(FilenameParts* pThis)
     size_t         filenameLength = pThis->filenameLength;
     struct dirent* pNextEntry;
     
-    DIR* pDir = opendir(pThis->pDirectoryName);
+    DIR* pDir = opendir(pThis->directoryName);
     if (!pDir)
-        return pThis->pActualFilename;
+        return pThis->actualFilename;
 
     while (NULL != (pNextEntry = readdir(pDir)))
     {
-        if (filenameLength == pNextEntry->d_namlen && 0 == strcasecmp(pNextEntry->d_name, pThis->pFilename))
+        if (filenameLength == pNextEntry->d_namlen && 0 == strcasecmp(pNextEntry->d_name, pThis->filename))
         {
-            memcpy(pThis->pActualFilename, pNextEntry->d_name, pNextEntry->d_namlen+1);
+            memcpy(pThis->actualFilename, pNextEntry->d_name, pNextEntry->d_namlen+1);
             break;
         }
     }
     closedir(pDir);
     
-    return pThis->pActualFilename;
-}
-
-static void freeFilenameParts(FilenameParts* pThis)
-{
-    free(pThis->pDirectoryName);
-    free(pThis->pFilename);
-    free(pThis->pActualFilename);
+    return pThis->actualFilename;
 }
